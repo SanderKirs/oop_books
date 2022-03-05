bf0d8fbba1ea86fe4af8f718cdc3a29a154828f4
SanderKirs <sander.kirs@khk.ee>
2022-03-05 19:23:05 +0200

    Refactored addbook functionality.

--- classes/UI.js
+++ classes/UI.js
@@ -1,19 +1,29 @@
 class UI{
+
+    addUIElement(name, classname = '', textcontent = '', attributes = {}){
+        const element = document.createElement('name');
+        if(classname !== ''){
+            element.className = classname
+        }
+        element.appendChild(document.createTextNode(textcontent));
+        if(Object.keys(attributes).length > 0) {
+            for(let key in attributes) {
+                element.setAttribute(key, attributes[key])
+            }
+        }
+        return element
+    }
+
     addBook(book){
-        const tr = document.createElement('tr');
+        const tr = this.addUIElement('tr');
 
         for(let name in book){
-            let td = document.createElement('td');
-            let text = document.createTextNode(book[name]);
-            td.appendChild(text);
+            let td = this.addUIElement('td', '', book[name]);
             tr.appendChild(td);
         }
 
-        let td = document.createElement('td');
-        const link = document.createElement('a');
-
-        link.setAttribute('href', '#');
-        link.appendChild(document.createTextNode('X'));
+        let td = this.addUIElement('td');
+        const link = this.addUIElement('a', '', 'X', {'href':'#'});
 
         td.appendChild(link);
         tr.appendChild(td);
